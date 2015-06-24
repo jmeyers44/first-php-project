0a24a73011ec6ad1d1e51c406a407d9ccdfaa348
Jonathan Meyers <jmeyers44@gmail.com>
2015-06-24 10:37:29 -0400

    create project

---
+++ resources/views/app.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Untitled</title>
+        <link rel="stylesheet" href="{{URL::asset('css/style.css') }}">
+        <link href='//fonts.googleapis.com/css?family=Lato:100' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        @yield('content');
+    </body>
+</html>
